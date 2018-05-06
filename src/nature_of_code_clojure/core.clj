c22cf3b85ac2ccefc90397fbf3a476796b7515e8
Rodrigo Alejandro Juarez <rodrigo.juarez.inf@gmail.com>
2018-05-06 11:24:54 +0200

    vector accelerated by mouse

--- src/nature_of_code_clojure/core.clj
+++ src/nature_of_code_clojure/core.clj
@@ -2,47 +2,45 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
+(use 'clojure.core.matrix)
+(use 'clojure.core.matrix.operators)
+(set-current-implementation :vectorz)
+
+(defn constrain-vector [a min max]
+  (let [constrained-vector (vector (q/constrain (nth a 0) min max) (q/constrain (nth a 1) min max))] constrained-vector))
+
 (defn setup []
   ; Set frame rate to 30 frames per second.
   (q/frame-rate 30)
-  ; Set color mode to HSB (HSV) instead of default RGB.
-  (q/color-mode :hsb)
-  ; setup function returns initial state. It contains
-  ; circle color and position.
-  {:color 0
-   :angle 0})
+  {:mouse (vector 0 0)
+   :velocity (vector 0 0)
+   :topSpeed 10
+   :location (vector (/ (q/width) 2) (/ (q/height) 2))})
 
 (defn update-state [state]
-  ; Update sketch state by changing circle color and position.
-  {:color (mod (+ (:color state) 0.7) 255)
-   :angle (+ (:angle state) 0.1)})
+  (let [mouse (vector (q/mouse-x) (q/mouse-y))
+        location (:location state)
+        direction (- mouse location)
+        dir (* (normalise direction) 0.5)
+        new-velocity (constrain-vector (+ dir (:velocity state)) -10 10)
+        new-location (+ new-velocity location)]
+    (assoc state :mouse mouse :velocity new-velocity :location new-location)))
 
 (defn draw-state [state]
   ; Clear the sketch by filling it with light-grey color.
   (q/background 240)
-  ; Set circle color.
-  (q/fill (:color state) 255 255)
-  ; Calculate x and y coordinates of the circle.
-  (let [angle (:angle state)
-        x (* 150 (q/cos angle))
-        y (* 150 (q/sin angle))]
-    ; Move origin point to the center of the sketch.
-    (q/with-translation [(/ (q/width) 2)
-                         (/ (q/height) 2)]
-      ; Draw the circle.
-      (q/ellipse x y 100 100))))
+  (q/ellipse (nth (:location state) 0) (nth (:location state) 1) 16 16))
 
-(defn -main [& args]
-  (q/defsketch nature-of-code-clojure
-    :title "You spin my circle right round"
-    :size [500 500]
+(q/defsketch nature-of-code-clojure
+  :title "You spin my circle right round"
+  :size [500 500]
     ; setup function called only once, during sketch initialization.
-    :setup setup
+  :setup setup
     ; update-state is called on each iteration before draw-state.
-    :update update-state
-    :draw draw-state
-    :features [:keep-on-top]
+  :update update-state
+  :draw draw-state
+  :features [:keep-on-top]
     ; This sketch uses functional-mode middleware.
     ; Check quil wiki for more info about middlewares and particularly
     ; fun-mode.
-    :middleware [m/fun-mode]))
+  :middleware [m/fun-mode])
