a36a71e5b0f04f8494bbe23918e79ba6e1a8b509
Rodrigo Alejandro Juarez <rodrigo.juarez.inf@gmail.com>
2018-05-06 12:12:49 +0200

    A weird horizontal snake

--- src/nature_of_code_clojure/core.clj
+++ src/nature_of_code_clojure/core.clj
@@ -2,46 +2,39 @@
   (:require [quil.core :as q]
             [quil.middleware :as m]))
 
+(use 'clojure.core.matrix)
+(use 'clojure.core.matrix.operators)
+(set-current-implementation :vectorz)
+
+(defn random-vec [min-a max-a min-b max-b]
+  (let [c (vector (q/random min-a max-a) (q/random min-b max-b))] c))
+
+(defn constrain-vector [a min-a max-a min-b max-b]
+  (let [constrained-vector (vector (q/constrain (nth a 0) min-a max-a) (q/constrain (nth a 1) min-b max-b))] constrained-vector))
+
 (defn setup []
-  ; Set frame rate to 30 frames per second.
   (q/frame-rate 30)
-  ; Set color mode to HSB (HSV) instead of default RGB.
-  (q/color-mode :hsb)
-  ; setup function returns initial state. It contains
-  ; circle color and position.
-  {:color 0
-   :angle 0})
+  {:snake-location (vector (q/width) (q/height))
+   :snake-velocity (vector 0.1 0.1)})
 
 (defn update-state [state]
-  ; Update sketch state by changing circle color and position.
-  {:color (mod (+ (:color state) 0.7) 255)
-   :angle (+ (:angle state) 0.1)})
+  (let [mouse (vector (q/mouse-x) (q/mouse-y))
+        snake-location (:snake-location state)
+        direction (* (normalise (- mouse (:snake-location state))) 0.5)
+        snake-velocity (constrain-vector (+ direction (:snake-velocity state)) -1 1 -5 5)
+        snake-location (+ snake-velocity snake-location)]
+    (assoc state :snake-location snake-location :snake-velocity snake-velocity)))
 
 (defn draw-state [state]
-  ; Clear the sketch by filling it with light-grey color.
   (q/background 240)
-  ; Set circle color.
-  (q/fill (:color state) 255 255)
-  ; Calculate x and y coordinates of the circle.
-  (let [angle (:angle state)
-        x (* 150 (q/cos angle))
-        y (* 150 (q/sin angle))]
-    ; Move origin point to the center of the sketch.
-    (q/with-translation [(/ (q/width) 2)
-                         (/ (q/height) 2)]
-      ; Draw the circle.
-      (q/ellipse x y 100 100))))
+  (q/ellipse (nth (:snake-location state) 0) (nth (:snake-location state) 1) 16 16))
 
 (q/defsketch nature-of-code-clojure
-  :title "You spin my circle right round"
+  :title "Eco eco eco"
   :size [500 500]
-    ; setup function called only once, during sketch initialization.
   :setup setup
-    ; update-state is called on each iteration before draw-state.
   :update update-state
   :draw draw-state
   :features [:keep-on-top]
-    ; This sketch uses functional-mode middleware.
-    ; Check quil wiki for more info about middlewares and particularly
-    ; fun-mode.
   :middleware [m/fun-mode])
+
